8efaa51deb1a7147a95b2ac5dfe853d68a08331b
LeChatong <ulrich.tchatong@gmail.com>
2020-03-06 11:21:30 +0100

    The First Commit !

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import {AuthService} from "../services/auth.service";
+import {Router} from "@angular/router";
+import {DataService} from "../services/data.service";
+import {UserModel} from "../services/model.service";
+import {map} from "rxjs/operators";
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
+})
+export class DashboardComponent implements OnInit {
+
+  CURRENT_USER: any;
+  user = {} as  UserModel;
+  constructor(private authService: AuthService, private router: Router, private dataService: DataService) {
+    this.CURRENT_USER = this.authService.currentUserValue;
+    this.user = (<UserModel>JSON.parse(this.CURRENT_USER.body));
+    //this.router.navigate(['user/home']);
+  }
+
+  ngOnInit() {
+
+  }
+
+  logout(){
+    this.authService.signOut();
+    this.router.navigate(['login']);
+  }
+}
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit } from '@angular/core';
+import {AuthService} from "../services/auth.service";
+import {Login, APIResponse, UserModel} from "../services/model.service";
+import {HttpResponse, HttpHeaders, HttpClient} from "@angular/common/http";
+import {DataService} from "../services/data.service";
+import {Router} from "@angular/router";
+import {first} from "rxjs/operators";
+
+declare var showLoader;
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+  login : Login;
+  headers : HttpHeaders;
+  authStatus: boolean;
+  message: string;
+
+
+  constructor(private authService: AuthService, private router: Router, private dataService: DataService) {
+    this.initView();
+    this.headers = new HttpHeaders();
+    this.message = '';
+  }
+
+  initView(){
+    this.login = {} as Login;
+  }
+  ngOnInit() {
+
+  }
+
+  onSignIn(){
+    showLoader(true);
+    this.authService.signIn(this.login.username, this.login.email)
+      .pipe(first())
+      .subscribe(
+        data =>{
+          showLoader(false);
+          this.router.navigate(['user/home']);
+        },
+        error1 => {
+          showLoader(false);
+          this.message = 'Erreur Serveur !';
+          location.reload();
+        }
+      );
+
+  }
+
+  onSignOut(){
+    this.authService.signOut();
+  }
+
+}
